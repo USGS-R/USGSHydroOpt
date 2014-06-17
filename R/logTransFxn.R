d2d856456a410c0a6efcb30a7a0dcd457597c6c8
Samuel T. Christel <schristel@wisc.edu>
2014-06-17 09:04:56 -0500

    Added the function "getLog10(dataSummary,signals,grnum)" which is contained in the script "logTransFxn.R" The example uses data which has already been loaded "dfsummary" and "ratioSignals." Documentation still needs to be improved for this function. I also changed a couple of the names for examples of the functions already loaded.

---
+++ R/logTransFxn.R
@@ -0,0 +1,30 @@
+#' getLog10
+#'
+#' Computes log transform of optical summary data. 
+#'
+#' @param dataSummary dataframe with summary absorbance and fluoresence data.
+#' @param signals vector of variable names in dataSummary for generating log transforms
+#' @param grnum Column name that defines the grnumbers in the dataSummary dataframe.
+#' These names are used to merge ratio data into the summary dataframe. 
+#' @export
+#' @examples
+#' dataSummary <- dfsummary
+#' signals <- ratioSignals[which(ratioSignals[2]>0),1]
+#' grnum<-"GRnumber"
+#' test2 <- getRatios(dataSummary=dfSummary,signals=signals)
+getLog10 <- function(dataSummary,signals,grnum){
+  
+  ratios <- data.frame(dataSummary[,grnum])
+  names(ratios) <- grnum
+  for(i in 1:(length(signals))){
+    dataSummary <- cbind(dataSummary,log10(dataSummary[,signals[i]]))
+    names(dataSummary)[dim(dataSummary)[2]] <- paste("log",signals[i],sep="")
+  
+  }
+  
+  return(dataSummary)
+  
+}
+    
+    
+    
