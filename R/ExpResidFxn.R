65c22ad4339e5df374c51b7d06db3f13ede7bbb1
Samuel T. Christel <schristel@wisc.edu>
2014-06-25 16:48:26 -0500

    Fixed dataAbs issue.

--- R/ExpResidFxn.R
+++ R/ExpResidFxn.R
@@ -68,6 +68,4 @@ getExpResid <- function(wavelength,rangeReg,rangeGap,dataAbs,waveCol,colSubsetSt
   dataSummaryFinal <- merge(dataSummary,dfResids,by=grnum,all=TRUE)
   return(dataSummaryFinal)
 }
-dataAbs <- dataAbs[,c(1:5,266)]
-dataSummary <- dataSummary[,-c(68)]
-dataSummary <- dataSummary[which(dataSummary$GRnumber=="gr13307"),]
+
