e312abe2024836ee3a27301f9a38bb59ce21f5ba
Steve Corsi <srcorsi@usgs.gov>
2014-11-07 17:09:14 -0600

    fixed bug in plotAbs

--- R/PlotAbsFxn.R
+++ R/PlotAbsFxn.R
@@ -5,18 +5,17 @@
 #' @param dataAbs dataframe of absorbance data
 #' @param WaveCol character column name to define the wavelengths for which absorbance was measured.
 #' @param absCol character column name to define which column to plot
-#' @param mainTitle Plot title
-#' @param titleSize font size for plot title
 #' @param ... additional plotting parameters as needed. xlim and ylim are commonly used here.
 #' @return Absorbance coefficient curve
 #' @export
 #' @examples
-#' WaveCol <- "Wavelength"
-#' absCol <- "Group002MantyOptical0006_20140606"
+#' WaveCol <- "wavelengths"
+#' absCol <- "gr13307"
 #' titleSize <- 1.1
 #' mainTitle <- "Example Absorbance Plot"
 #' xlim <- c(239,450)
-#' plotAbs(dataAbs,WaveCol,AbsCol,main=mainTitle,cex.main=titleSize,xlim=xlim)
+#' dataAbs <- dfabs
+#' plotAbs(dataAbs,WaveCol,absCol,main=mainTitle,cex.main=titleSize,xlim=xlim)
 
 plotAbs <- function(dataAbs,WaveCol,absCol,...){
 
